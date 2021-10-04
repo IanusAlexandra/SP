290b8467e19850fb60784c8b088efb9d27755546
AlexandraIanus <59911710+IanusAlexandra@users.noreply.github.com>
2021-10-04 20:53:25 +0300

    Laborator 2

---
+++ Lab2/src/Image.java
@@ -0,0 +1,14 @@
+public class Image implements  Element {
+
+    public String imageName;
+    public Image(String imageName){
+        this.imageName=imageName;
+
+    }
+
+    public  void print(){
+        System.out.println(this.imageName);
+    }
+
+
+}
